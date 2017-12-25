0e7ef55af2fe8dc2fef2955bd11940e059a62809
Arvid Björkqvist <arvid.bjorkqvist@gmail.com>
2017-12-25 11:29:59 +0100

    Added application logs for startup/shutdown and unhandlex exceptions

--- QuickPaste.Net/App.xaml.cs
+++ QuickPaste.Net/App.xaml.cs
@@ -1,11 +1,14 @@
 ﻿using Hardcodet.Wpf.TaskbarNotification;
+using NLog;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace QuickPaste.Net
 {
@@ -16,9 +19,60 @@ namespace QuickPaste.Net
     /// </summary>
     public partial class App : Application
     {
+        private static Logger Logger => LogManager.GetCurrentClassLogger();
 
-        public App()
+        private void Application_Startup(object sender, StartupEventArgs e)
         {
+            try
+            {
+                var version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+                Logger.Info($"Starting QuickPaste {version}" +
+#if DEBUG
+                    " Debug" +
+#endif
+                    $" - {Environment.OSVersion.VersionString}");
+
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex);
+            }
+
+            DispatcherUnhandledException += AppDispatcherUnhandledException;
+            Dispatcher.UnhandledException += Dispatcher_UnhandledException;
+            TaskScheduler.UnobservedTaskException += OnUnobservedTaskException;
+        }
+
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            Logger.Info("Shutdown QuickPaste");
+        }
+
+        private void OnUnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            e.SetObserved();
+            CurrentDomainUnhandledException(sender, new UnhandledExceptionEventArgs(e.Exception, false));
+        }
+
+        private void Dispatcher_UnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            e.Handled = true;
+            CurrentDomainUnhandledException(sender, new UnhandledExceptionEventArgs(e.Exception, false));
+        }
+
+
+        private void AppDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            e.Handled = true;
+            CurrentDomainUnhandledException(sender, new UnhandledExceptionEventArgs(e.Exception, false));
+        }
+
+        private static void CurrentDomainUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            if (e.ExceptionObject is Exception ex)
+            {
+                Logger.Fatal(ex);
+            }
         }
     }
 }
